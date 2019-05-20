f4272e884d6b1d290ce5875101981d4b32d196c1
Antti Leppä <antti.leppa@metatavu.fi>
2019-05-20 11:47:40 +0300

    Generated javascript source

--- typescript-client-generated/model/delivery.ts
+++ typescript-client-generated/model/delivery.ts
@@ -9,7 +9,7 @@
  * https://github.com/swagger-api/swagger-codegen.git
  * Do not edit the class manually.
  */
-import { DeliveryLoanable } from './deliveryLoanable';
+import { DeliveryLoan } from './deliveryLoan';
 import { DeliveryStatus } from './deliveryStatus';
 
 
@@ -29,7 +29,7 @@ export interface Delivery {
     /**
      * Array of loans. Property is write only
      */
-    loans?: Array<DeliveryLoanable>;
+    loans?: Array<DeliveryLoan>;
 }
 export interface DeliveryOpt { 
     id?: string;
@@ -47,5 +47,5 @@ export interface DeliveryOpt {
     /**
      * Array of loans. Property is write only
      */
-    loans?: Array<DeliveryLoanable>;
+    loans?: Array<DeliveryLoan>;
 }
