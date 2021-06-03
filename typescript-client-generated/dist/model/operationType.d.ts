2ce2b81bf6b4ade678e53f323459fac964096853
Lauri Lukkarinen <lauri.lukkarinen@metatavu.fi>
2021-06-03 12:07:55 +0300

    Added new operation

--- typescript-client-generated/dist/model/operationType.d.ts
+++ typescript-client-generated/dist/model/operationType.d.ts
@@ -9,7 +9,7 @@
  * https://github.com/swagger-api/swagger-codegen.git
  * Do not edit the class manually.
  */
-export declare type OperationType = 'SAP_CONTACT_SYNC' | 'SAP_DELIVERY_PLACE_SYNC' | 'SAP_ITEM_GROUP_SYNC' | 'SAP_CONTRACT_SYNC' | 'SAP_CONTRACT_SAPID_SYNC' | 'ITEM_GROUP_DEFAULT_DOCUMENT_TEMPLATES';
+export declare type OperationType = 'SAP_CONTACT_SYNC' | 'SAP_DELIVERY_PLACE_SYNC' | 'SAP_ITEM_GROUP_SYNC' | 'SAP_CONTRACT_SYNC' | 'SAP_CONTRACT_SAPID_SYNC' | 'ITEM_GROUP_DEFAULT_DOCUMENT_TEMPLATES' | 'UPDATE_CURRENT_YEAR_APPROVED_CONTRACTS_TO_SAP';
 export declare const OperationType: {
     SAPCONTACTSYNC: OperationType;
     SAPDELIVERYPLACESYNC: OperationType;
@@ -17,4 +17,5 @@ export declare const OperationType: {
     SAPCONTRACTSYNC: OperationType;
     SAPCONTRACTSAPIDSYNC: OperationType;
     ITEMGROUPDEFAULTDOCUMENTTEMPLATES: OperationType;
+    UPDATECURRENTYEARAPPROVEDCONTRACTSTOSAP: OperationType;
 };
