6c61d16e2cc43383eb392eae9e8a3f4270bf7976
Lauri Lukkarinen <lauri.lukkarinen@metatavu.fi>
2021-06-17 15:54:05 +0300

    Generated javascript source

--- typescript-client-generated/model/body1.ts
+++ typescript-client-generated/model/body1.ts
@@ -9,16 +9,11 @@
  * https://github.com/swagger-api/swagger-codegen.git
  * Do not edit the class manually.
  */
-import { DeliveryLoan } from './deliveryLoan';
 
 
 export interface Body1 { 
-    contactId: string;
-    comment: string;
-    loans: Array<DeliveryLoan>;
+    file?: string;
 }
 export interface Body1Opt { 
-    contactId?: string;
-    comment?: string;
-    loans?: Array<DeliveryLoan>;
+    file?: string;
 }
